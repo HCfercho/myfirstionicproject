e28c709d8af403f291e9a22e8f9a819b7f09b9f8
Fernando <carferchomen@gmail.com>
2020-05-17 23:22:31 -0500

    project created
    The project was created in vscode and use capacitor

---
+++ src/app/places/place-detail/place-detail.page.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
+import { AlertController } from "@ionic/angular";
+
+@Component({
+  selector: 'app-place-detail',
+  templateUrl: './place-detail.page.html',
+  styleUrls: ['./place-detail.page.scss'],
+})
+export class PlaceDetailPage implements OnInit {
+
+  place: Place
+
+  constructor(private activatedRouter: ActivatedRoute, private placesService: PlacesService, private router: Router, private alertCtrl: AlertController) { }
+
+  ngOnInit() {
+    this.activatedRouter.paramMap.subscribe(paramMap => {
+      
+        // redirect
+        const recipeId = paramMap.get('placeId');
+        this.place = this.placesService.getPlace(recipeId);
+    })
+  }
+
+  async deletePlace(){
+
+    const alertElement = await this.alertCtrl.create({
+      header: 'Are you sure, you want to delete it?',
+      message: 'Be carefull.',
+      buttons: [{
+        text: 'Cancel',
+        role: 'Cancel'
+      },
+      {
+        text:'Delete',
+        handler: () => {
+          this.placesService.deletePlace(this.place.id);
+          this.router.navigate(['/places']);
+        }
+      }
+    ]
+    });
+    await alertElement.present();
+    
+  }
+
+}
